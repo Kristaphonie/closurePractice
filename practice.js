4a89f13a9307fbd1b071d105736aea6f56ce6e86
Kristaphonie <csaunders@getjive.com>
2015-04-16 19:21:41 -0600

    finished project

--- practice.js
+++ practice.js
@@ -6,6 +6,10 @@ var outer = function(){
     return 'The original name was ' + name;
   }
 }
+
+var inner = outer();
+
+inner();
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
@@ -29,8 +33,11 @@ var callFriend = function(){
   return callF;
 };
 
+callFriend()('435-215-9248');
+
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
+
 
   //Code Here
 
@@ -45,11 +52,30 @@ var callFriend = function(){
 */
 
   //Code Here
+
+  function makeCounter()
+  {
+    return function ()
+    {
+      console.log("1");
+      return function ()
+      {
+        console.log("2");
+        return function ()
+        {
+          console.log("3");
+          return function ()
+          {
+            return console.log("4");
+          }
+        }
+      }
+    }
+  }
+
   var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+  count()()()() // 1
+
 
 
 
@@ -63,6 +89,28 @@ var callFriend = function(){
   Once completed, add a second arguments that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
 */
+
+function firstFunc (secondFunc, n)
+{
+  return function ()
+  {
+    for (var i = 0; i < n; i++)
+    {
+      secondFunc();
+    }
+    console.log('STAHHP');
+  }
+}
+
+var secondFunc = function ()
+{
+  console.log("Yipee!");
+}
+
+var run = firstFunc(secondFunc, 5);
+
+run()
+
 
 
 
